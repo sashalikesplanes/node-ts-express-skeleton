7ba0583065069a1d7408f5a8bef0a28795e17f13
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-08-25 10:53:26 +0200

    added docker support

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:lts-alpine
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+
+# For prod
+# RUN npm ci --only=production
+
+COPY . .
+EXPOSE 3000
+RUN npm run build
+CMD ["node", "dist/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-const name = 'bob';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-console.log('Hello ', name);
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  return res.send({
+    message: 'I am working v3',
+  });
+});
+
+app.post('', async (req, res) => {
+  console.log(req.body);
+  return res.send({
+    message: 'Hello from post',
+  });
+});
+
+const PORT = 3000;
+
+try {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
